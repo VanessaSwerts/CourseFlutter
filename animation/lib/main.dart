7b37814dce3b24e99e3ebc6e6736aceb06839eef
VanessaSwerts <vanessaswerts@gec.inatel.br>
2020-05-29 20:50:45 -0300

    "Começando app, criando tela LoginScreen"

--- animation/lib/main.dart
+++ animation/lib/main.dart
@@ -1,113 +1,16 @@
+import 'package:animation/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-
-class LogoApp extends StatefulWidget {
-  @override
-  _LogoAppState createState() => _LogoAppState();
-}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Animations Intro",
+      title: "Animations",
       debugShowCheckedModeBanner: false,
-      home: LogoApp(),
+      home: LoginScreen(),
     );
   }
 }
 
-class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
-  AnimationController controller;
-  Animation<double> animation;
-  @override
-  void initState() {
-    super.initState();
-
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 2),
-    );
-
-    animation = CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn);
-    animation.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        controller.reverse();
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
-
-    controller.forward();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimationTransition(
-      animation: animation,
-      child: LogoWidget(),
-    );
-  }
-}
-
-// class AnimatedLogo extends AnimatedWidget {
-//   AnimatedLogo(Animation<double> animation) : super(listenable: animation);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     Animation<double> animation = listenable;
-//     return Center(
-//       child: Container(
-//         height: animation.value,
-//         width: animation.value,
-//         child: FlutterLogo(),
-//       ),
-//     );
-//   }
-// }
-
-class LogoWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: FlutterLogo(),
-    );
-  }
-}
-
-class AnimationTransition extends StatelessWidget {
-  final Widget child;
-  final Animation<double> animation;
-
-  final sizeTween = Tween<double>(begin: 0, end: 300);
-  final opacityTween = Tween<double>(begin: 0.1, end: 1);
-
-  AnimationTransition({this.child, this.animation});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: AnimatedBuilder(
-        animation: animation,
-        builder: (context, child) {
-          return Opacity(
-            opacity: opacityTween.evaluate(animation).clamp(0.0, 1.0),
-            child: Container(
-              height: sizeTween.evaluate(animation),
-              width: sizeTween.evaluate(animation),
-              child: child,
-            ),
-          );
-        },
-        child: child,
-      ),
-    );
-  }
-}
